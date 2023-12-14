1e76e7da3c932cb31be3e8ae318f20ecdd5f6283
pontuagi <benepontuagi@gmail.com>
2023-12-14 14:50:51 +0000

    Optimize search and store correction

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,22 +6,9 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_count INT;
-    DECLARE average_score FLOAT;
-
-    SELECT SUM(score), COUNT(*) 
-    INTO total_score, total_count 
-    FROM corrections 
-    WHERE user_id = user_id;
-
-    IF total_count > 0 THEN
-        SET average_score = total_score / total_count;
-
-        UPDATE users 
-        SET average_score = average_score 
-        WHERE id = user_id;
-    END IF;
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
 END //
 
 DELIMITER ;
