ab37c9b28f8c7c395cab653431c667d9df40afb6
pontuagi <benepontuagi@gmail.com>
2023-12-14 10:02:40 +0000

    safe divide correction

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,21 @@
 -- Create the SafeDiv function
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
 DELIMITER //
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT
 BEGIN
-    DECLARE result INT;
+    DECLARE result FLOAT;
+
     IF b = 0 THEN
         SET result = 0;
     ELSE
         SET result = a / b;
     END IF;
+
     RETURN result;
 END //
+
 DELIMITER ;
--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,6 @@
+-- script that creates a view need_meeting that lists all students 
+-- that have a score under 80 (strict) and no last_meeting or more than 1 month.
+
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
