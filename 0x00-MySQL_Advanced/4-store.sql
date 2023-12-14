912c5bd7e7c31dcb7d9f4550af900fbc329a5953
pontuagi <benepontuagi@gmail.com>
2023-12-14 06:54:03 +0000

    Trigger correction

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,19 @@
---  script that creates a trigger that decreases the quantity of an item after adding a new order.
+--  script that creates a trigger that decreases the quantity of an item after
 DELIMITER //
+
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+    DECLARE item_name VARCHAR(255); -- Use the appropriate data type
+
+    -- Get the item name from the new order
+    SET item_name = NEW.item_name;
+
+    -- If the item exists, update its quantity
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
+    WHERE name = item_name;
+
 END;
 //
 DELIMITER ;
