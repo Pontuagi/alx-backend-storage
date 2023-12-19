1589fc81bf65aefc08c4cfabc415b3b6d560801f
Pontuagi <benepontuagi@gmail.com>
2023-12-19 11:52:33 +0300

    top student

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function top_students(mongo_collection)
+pymongo
+"""
+
+
+def top_student(mongo_collection):
+    """
+    A function that returns all students sorted bu average score
+
+    mongo_collection: pymongo collection object
+    """
+    pipeline = [
+        {
+            "$project": {
+                "_id": 1,
+                "name": 1,
+                "scores": 1,
+                "averageScore": { "$avg": "$scores.score" }
+            }
+        },
+        { "$sort": { "averageScore": -1 } }
+    ]
+
+    top_students = list(mongo_collection.aggregate(pipeline))
+
+    return top_students
